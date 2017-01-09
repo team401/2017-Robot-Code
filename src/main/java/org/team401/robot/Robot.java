3f0e0cdf38ed85b9cb9435a205179a9d0400ad69
Zach Kozar <zachkozar@masterzach32.net>
2017-01-09 18:13:01 -0500

    Overrided robot functions

--- src/main/java/org/team401/robot/Robot.java
+++ src/main/java/org/team401/robot/Robot.java
@@ -4,4 +4,34 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Robot extends IterativeRobot {
 
+    @Override
+    public void robotInit() {
+
+    }
+
+    @Override
+    public void autonomousInit() {
+
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+
+    }
+
+    @Override
+    public void teleopInit() {
+
+    }
+
+    @Override
+    public void teleopPeriodic() {
+
+    }
+
+    @Override
+    public void disabledInit() {}
+
+    @Override
+    public void disabledPeriodic() {}
 }
