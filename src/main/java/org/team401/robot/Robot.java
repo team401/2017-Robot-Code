4f6bb3e7fea46a795c1a62accf8b6c54ffaf638f
Zach Kozar <zachkozar@masterzach32.net>
2017-01-12 18:13:23 -0500

    Added test code

--- src/main/java/org/team401/robot/Robot.java
+++ src/main/java/org/team401/robot/Robot.java
@@ -1,17 +1,34 @@
 package org.team401.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import org.strongback.Strongback;
+import org.strongback.components.Motor;
+import org.strongback.components.ui.FlightStick;
+import org.strongback.drive.TankDrive;
+import org.strongback.hardware.Hardware;
 
 public class Robot extends IterativeRobot {
 
+    private TankDrive allDrive;
+    private FlightStick joysticky;
+
     @Override
     public void robotInit() {
+        Strongback.configure()
+                .recordDataToFile("/home/lvuser/")
+                .recordEventsToFile("/home/lvuser/", 2097152);
 
+        Motor leftDrive = Hardware.Motors.talon(0);
+        Motor rightDrive = Hardware.Motors.talon(1);
+
+        allDrive = new TankDrive(leftDrive, rightDrive);
+
+        joysticky = Hardware.HumanInterfaceDevices.logitechAttack3D(0);
     }
 
     @Override
     public void autonomousInit() {
-
+        Strongback.start();
     }
 
     @Override
@@ -21,16 +38,18 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void teleopInit() {
-
+        Strongback.restart();
     }
 
     @Override
     public void teleopPeriodic() {
-
+        allDrive.arcade(joysticky.getPitch().read(), joysticky.getRoll().read());
     }
 
     @Override
-    public void disabledInit() {}
+    public void disabledInit() {
+        Strongback.disable();
+    }
 
     @Override
     public void disabledPeriodic() {}
