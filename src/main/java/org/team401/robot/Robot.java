4363240ffb7c5b6d7de8598d2a081936e536c08b
Briangulator <brian174174@gmail.com>
2017-01-14 17:12:23 -0500

    Added example project

--- src/main/java/org/team401/robot/Robot.java
+++ src/main/java/org/team401/robot/Robot.java
@@ -1,86 +1,114 @@
+/**
+ * This Java FRC robot application is meant to demonstrate an example using the Motion Profile control mode
+ * in Talon SRX.  The CANTalon class gives us the ability to buffer up trajectory points and execute them
+ * as the roboRIO streams them into the Talon SRX.
+ * 
+ * There are many valid ways to use this feature and this example does not sufficiently demonstrate every possible
+ * method.  Motion Profile streaming can be as complex as the developer needs it to be for advanced applications,
+ * or it can be used in a simple fashion for fire-and-forget actions that require precise timing.
+ * 
+ * This application is an IterativeRobot project to demonstrate a minimal implementation not requiring the command 
+ * framework, however these code excerpts could be moved into a command-based project.
+ * 
+ * The project also includes instrumentation.java which simply has debug printfs, and a MotionProfile.java which is generated
+ * in @link https://docs.google.com/spreadsheets/d/1PgT10EeQiR92LNXEOEe3VGn737P7WDP4t0CQxQgC8k0/edit#gid=1813770630&vpid=A1
+ * 
+ * Logitech Gamepad mapping, use left y axis to drive Talon normally.  
+ * Press and hold top-left-shoulder-button5 to put Talon into motion profile control mode.
+ * This will start sending Motion Profile to Talon while Talon is neutral. 
+ * 
+ * While holding top-left-shoulder-button5, tap top-right-shoulder-button6.
+ * This will signal Talon to fire MP.  When MP is done, Talon will "hold" the last setpoint position
+ * and wait for another button6 press to fire again.
+ * 
+ * Release button5 to allow OpenVoltage control with left y axis.
+ */
+
 package org.team401.robot;
 
 import com.ctre.CANTalon;
+import com.ctre.CANTalon.TalonControlMode;
 import edu.wpi.first.wpilibj.IterativeRobot;
-
-import org.strongback.Strongback;
-import org.strongback.components.Motor;
-import org.strongback.components.TalonSRX;
-import org.strongback.components.ui.FlightStick;
-import org.strongback.drive.TankDrive;
-import org.strongback.hardware.Hardware;
+import edu.wpi.first.wpilibj.Joystick;
 
 public class Robot extends IterativeRobot {
 
-    private TankDrive allDrive;
-    private FlightStick joysticky;
-    private Motor collection;// This is for the collection method proposed by the Ethan Manipulators
-    private final double collectSpeed = 0.5;
+	/** The Talon we want to motion profile. */
+	CANTalon _talon = new CANTalon(9);
 
-    CANTalon _talon;
-    @Override
-    public void robotInit() {
-        Strongback.configure()
-                .recordDataToFile("/home/lvuser/")
-                .recordEventsToFile("/home/lvuser/", 2097152);
+	/** some example logic on how one can manage an MP */
+	MotionProfileExample _example = new MotionProfileExample(_talon);
+	
+	/** joystick for testing */
+	Joystick _joy= new Joystick(0);
 
-        Motor leftDrive = Hardware.Motors.talon(0);
-        Motor rightDrive = Hardware.Motors.talon(1);
+	/** cache last buttons so we can detect press events.  In a command-based project you can leverage the on-press event
+	 * but for this simple example, lets just do quick compares to prev-btn-states */
+	boolean [] _btnsLast = {false,false,false,false,false,false,false,false,false,false};
 
-        allDrive = new TankDrive(leftDrive, rightDrive);
 
-        collection = Hardware.Motors.talon(2);
-
-        joysticky = Hardware.HumanInterfaceDevices.logitechAttack3D(0);
-
-        _talon = new CANTalon(9);
-    }
-
-    @Override
-    public void autonomousInit() {
-        Strongback.start();
-    }
-
-    @Override
-    public void autonomousPeriodic() {
-        collection.setSpeed(collectSpeed);
-    }
-
-    @Override
-    public void teleopInit() {
-        Strongback.restart();
-    }
-
-    @Override
+	public Robot() { // could also use RobotInit()
+		_talon.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Relative);
+		_talon.reverseSensor(false); /* keep sensor and motor in phase */
+	}
+	/**  function is called periodically during operator control */
     public void teleopPeriodic() {
-        //drive
-        allDrive.arcade(joysticky.getPitch().read(), joysticky.getRoll().read());
-        collection.setSpeed(collectSpeed);
+		/* get buttons */
+		boolean [] btns= new boolean [_btnsLast.length];
+		for(int i=1;i<_btnsLast.length;++i)
+			btns[i] = _joy.getRawButton(i);
 
-        //if the trigger is pressed the motor can spin If released it stops
-        //uses voltage as the controller
-        if(joysticky.getTrigger().isTriggered()){
-            _talon.enable();
-            _talon.changeControlMode(CANTalon.TalonControlMode.Voltage);
-            _talon.set(12.0 * joysticky.getPitch().read());
-        }else if(!joysticky.getTrigger().isTriggered()){
-          _talon.disable();
-        }
-        //sets the F Gain
-        _talon.getSpeed();
-        double throttle = joysticky.getPitch().read()/1;
-        double FGain = ((throttle/1023) * _talon.getSpeed());
-        _talon.setF(FGain);
-    }
+		/* get the left joystick axis on Logitech Gampead */
+		double leftYjoystick = -1 * _joy.getY(); /* multiple by -1 so joystick forward is positive */
 
-    @Override
-    public void disabledInit() {
-        Strongback.disable();
-        collection.setSpeed(0);
-    }
+		/* call this periodically, and catch the output.  Only apply it if user wants to run MP. */
+		_example.control();
+		
+		if (btns[5] == false) { /* Check button 5 (top left shoulder on the logitech gamead). */
+			/*
+			 * If it's not being pressed, just do a simple drive.  This
+			 * could be a RobotDrive class or custom drivetrain logic.
+			 * The point is we want the switch in and out of MP Control mode.*/
+		
+			/* button5 is off so straight drive */
+			_talon.changeControlMode(TalonControlMode.Voltage);
+			_talon.set(12.0 * leftYjoystick);
 
-    @Override
-    public void disabledPeriodic() {}
+			_example.reset();
+		} else {
+			/* Button5 is held down so switch to motion profile control mode => This is done in MotionProfileControl.
+			 * When we transition from no-press to press,
+			 * pass a "true" once to MotionProfileControl.
+			 */
+			_talon.changeControlMode(TalonControlMode.MotionProfile);
+			
+			CANTalon.SetValueMotionProfile setOutput = _example.getSetValue();
+					
+			_talon.set(setOutput.value);
 
+			/* if btn is pressed and was not pressed last time,
+			 * In other words we just detected the on-press event.
+			 * This will signal the robot to start a MP */
+			if( (btns[6] == true) && (_btnsLast[6] == false) ) {
+				/* user just tapped button 6 */
+				_example.startMotionProfile();
+			}
+		}
 
+		/* save buttons states for on-press detection */
+		for(int i=1;i<10;++i)
+			_btnsLast[i] = btns[i];
+
+	}
+	/**  function is called periodically during disable */
+	public void disabledPeriodic() {
+		/* it's generally a good idea to put motor controllers back
+		 * into a known state when robot is disabled.  That way when you
+		 * enable the robot doesn't just continue doing what it was doing before.
+		 * BUT if that's what the application/testing requires than modify this accordingly */
+		_talon.changeControlMode(TalonControlMode.PercentVbus);
+		_talon.set( 0 );
+		/* clear our buffer and put everything into a known state */
+		_example.reset();
+	}
 }
