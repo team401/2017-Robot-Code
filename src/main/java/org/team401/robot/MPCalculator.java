c8ab12584af706ae5dc1cf51e35571bfa20bff85
Unknown <eridor@verizon.net>
2017-01-16 17:52:08 -0500

    Started adding a model of the 2017 field

--- src/main/java/org/team401/robot/MPCalculator.java
+++ src/main/java/org/team401/robot/MPCalculator.java
@@ -1,5 +1,6 @@
 package org.team401.robot;
 
+import java.awt.*;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,19 +10,46 @@ import java.io.IOException;
  */
 public class MPCalculator {
     public static void main(String[] args){
+        //numbers are in feet
         double[][] path = new double[][]{
-                {0, 0},
-                {5, 10},
-                {10, 10},
+                {20, 0},
                 {20, 10},
-                {50, 30}
+                {10, 10},
+                {10, 5},
+                {10, 0}
         };
-        FalconLinePlot gui = new FalconLinePlot(path);
-        gui.xGridOn();
-        gui.yGridOn();
+        double[][] path2 = new double[][]{
+                {1,1},
+                {1,1},
+                {1,1},
+                {1,1},
+                {1,1},
+        };
+        FalconLinePlot fig1 = new FalconLinePlot(path);
+        fig1.xGridOn();
+        fig1.yGridOn();
+        fig1.setTitle("Figure 1");
+        fig1.setXLabel("X (feet)");
+        fig1.setYLabel("Y (feet)");
+        //Field map
+        FalconLinePlot fig2 = new FalconLinePlot(path2);
+        fig2.xGridOn();
+        fig2.yGridOn();
+        fig2.setTitle("2017 Field Map");
+        fig2.setXLabel("Width of the Field (feet)");
+        fig2.setYLabel("Length of the Field (feet)");
+        fig2.setXTic(0, 54 , 1);
+        fig2.setYTic(0, 27, 1);
+        
+
+
         FalconPathPlanner falcon = new FalconPathPlanner(path);
-        falcon.calculate(20, 0.02, 26);
-        while(gui.isFocusable()){
+        //in feet
+        falcon.calculate(20, 0.02, 2.16666);
+        fig1.addData(falcon.smoothPath, Color.red, Color.blue);
+        fig1.addData(falcon.leftPath, Color.magenta);
+        fig1.addData(falcon.rightPath, Color.magenta);
+        while(fig1.isFocusable()){
 
         }
 
