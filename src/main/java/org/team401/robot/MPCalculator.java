5244ef40f0c5379b65e9ef959a07e1e5d380adb1
Unknown <eridor@verizon.net>
2017-01-18 20:34:12 -0500

    Added the airships to the map

--- src/main/java/org/team401/robot/MPCalculator.java
+++ src/main/java/org/team401/robot/MPCalculator.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.Math;
 
 /**
  * Created by Brian Jameson on 1/16/2017.
@@ -20,28 +21,51 @@ public class MPCalculator {
         };
         double[][] path2 = new double[][]{
                 {1,1},
-                {1,1},
-                {1,1},
-                {1,1},
-                {1,1},
+                {1,2},
+                {1,3},
+                {1,4},
+                {1,5},
         };
+        //draws the airships:
+        //side length = 3 ft
+        //3.232
+        //1.5
+        //hypotenuse : 3 ft
+        //short leg : 1.5 ft
+        //long leg : 2.5980762 ft
+        //distance between airships : 23.518 ft
+        double airshipSpace = 23.518;
+        double side = 1.5 * Math.sqrt(3);
+        double [][] airship = new double [][]{
+                {12, 10},
+                {(12 - side), 11.5},
+                {(12 - side), 14.5},
+                {12, 16},
+                {12 + side, 14.5},
+                {12 + side, 11.5},
+                {12, 10}
+
+        };
+        double [][] airship2 = new double [][]{
+                {12 + airshipSpace, 10},
+                {(12 - side) + airshipSpace, 11.5},
+                {(12 - side) + airshipSpace, 14.5},
+                {12 + airshipSpace, 16},
+                {12 + side + airshipSpace, 14.5},
+                {12 + side + airshipSpace, 11.5},
+                {12 + airshipSpace, 10}
+
+        };
+
+
+
         FalconLinePlot fig1 = new FalconLinePlot(path);
         fig1.xGridOn();
         fig1.yGridOn();
         fig1.setTitle("Figure 1");
         fig1.setXLabel("X (feet)");
         fig1.setYLabel("Y (feet)");
-        //Field map
-        FalconLinePlot fig2 = new FalconLinePlot(path2);
-        fig2.xGridOn();
-        fig2.yGridOn();
-        fig2.setTitle("2017 Field Map");
-        fig2.setXLabel("Width of the Field (feet)");
-        fig2.setYLabel("Length of the Field (feet)");
-        fig2.setXTic(0, 54 , 1);
-        fig2.setYTic(0, 27, 1);
-        
-
+        //fig1.addData(box, Color.black);
 
         FalconPathPlanner falcon = new FalconPathPlanner(path);
         //in feet
@@ -49,11 +73,32 @@ public class MPCalculator {
         fig1.addData(falcon.smoothPath, Color.red, Color.blue);
         fig1.addData(falcon.leftPath, Color.magenta);
         fig1.addData(falcon.rightPath, Color.magenta);
+
+        //Field map
+        FalconLinePlot fig2 = new FalconLinePlot(path2);
+        fig2.xGridOn();
+        fig2.yGridOn();
+        fig2.setTitle("2017 Field Map");
+        fig2.setXLabel("Width of the Field (feet)");
+        fig2.setYLabel("Length of the Field (feet)");
+        //filed size: x: 54 ft y: 27 ft
+        fig2.setXTic(0, 54 , 1);
+        fig2.setYTic(0, 27, 1);
+
+
+        fig2.addData(airship, Color.black);
+        fig2.addData(airship2, Color.black);
+
+
+
+
+
         while(fig1.isFocusable()){
 
         }
 
     }
+
     public static void write(String filename, double[][] arr) throws IOException{
         BufferedWriter output = new BufferedWriter(new FileWriter(filename));
         for(int i = 0; i < arr.length; i++){
