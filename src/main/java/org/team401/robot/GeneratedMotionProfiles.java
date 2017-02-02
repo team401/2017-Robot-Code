cbb1d5cd54f308acaba657ee77244890e43b62ae
Briangulator <brian174174@gmail.com>
2017-02-01 19:56:13 -0500

    Scans from .csv now

--- src/main/java/org/team401/robot/GeneratedMotionProfiles.java
+++ src/main/java/org/team401/robot/GeneratedMotionProfiles.java
@@ -1,5 +1,10 @@
 package org.team401.robot;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class GeneratedMotionProfiles {
 	// Position (rotations)	Velocity (RPM)	Duration (ms)
 	public static double[][][] getProfile(int start, int tgt, boolean mecanum) {
@@ -103,6 +108,35 @@ public class GeneratedMotionProfiles {
 				default:
 					return new double[0][0][0];//Return empty array to sit still if something bad happened
 			}
+	}
+
+	public double[][] scanCSV(String fileName){
+		BufferedReader br = null;
+		String line;
+		double[][] result;
+		String[][] strs = new String[99999][3];
+		try {
+			br = new BufferedReader(new FileReader(fileName));
+			int i = 1;
+			while ((line = br.readLine()) != null) {
+
+				// use comma as separator
+				strs[i] = line.split(",");
+				i++;
+			}
+			result = new double[i][3];
+			for(int j = 0; j < i; j++)
+				for(int k = 0; k < 3; k++)
+					result[j][k] = Double.parseDouble(strs[j][k]);
+			br.close();
+			return result;
+		} catch (FileNotFoundException e) {
+			System.out.println("File " + fileName + "not found!");
+		} catch (IOException e) {
+			System.out.println("IOException in scanCSV while scanning " + fileName + "!");
+		}
+		return new double[0][0];
+
 	}
 
 	public static final double[][][] mecanumMidCenterLift = {
