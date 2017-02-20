790577ec66dd2182ad43fa4cb7823e6f1cbffe8c
Zach Kozar <zachkozar@masterzach32.net>
2017-02-20 12:46:57 -0500

    Added follow path action

---
+++ src/main/java/org/team401/robot/auto/actions/FollowPathAction.java
@@ -0,0 +1,28 @@
+package org.team401.robot.auto.actions;
+
+public class FollowPathAction implements Action {
+
+    public FollowPathAction() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end() {
+
+    }
+}
