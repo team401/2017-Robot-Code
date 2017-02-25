c03f1fa36d0e1071df8e4f08a50478b23dc24cee
Briangulator <brian174174@gmail.com>
2017-02-24 19:42:20 -0500

    Still early for CA class

--- src/main/java/org/team401/robot/auto/modes/CompetitionAuto.java
+++ src/main/java/org/team401/robot/auto/modes/CompetitionAuto.java
@@ -5,27 +5,50 @@ import org.team401.robot.auto.AutoMode;
 import org.team401.robot.auto.actions.CalibrateTurretAction;
 import org.team401.robot.auto.actions.DriveStraightAction;
 import org.team401.robot.auto.actions.FollowPathAction;
+import org.team401.robot.auto.actions.RotateAction;
 
+/**
+ * This is early code. Replace all numbers in the class, as they are zeroed right now!
+ */
 public class CompetitionAuto extends AutoMode {
-	public enum Mode{
-		MIDGEAR, LEFTGEAR, RIGHTGEAR, REDHOPPER, BLUHOPPER
+	public enum Destination{
+		MIDGEAR, LEFTGEAR, RIGHTGEAR, LEFTHOPPER, RIGHTHOPPER
 	}
-	private Mode currentMode;
+	private Destination destination;
+	private boolean team;
+	public CompetitionAuto(Destination destination, boolean team){
+		this.destination = destination;
+		this.team = team;
+	}
 	public void routine() {
 		runAction(new CalibrateTurretAction());
-		switch(currentMode) {
+		switch(destination) {
 			case MIDGEAR:
-				runAction(new DriveStraightAction(0, 0, 0));//Replace all measurements!
+				runAction(new DriveStraightAction(0, 0, 0));//Drive onto the peg
 				break;
 			case LEFTGEAR:
+				runAction(new DriveStraightAction(0, 0, 0));//Drive in front of the peg
+				runAction(new RotateAction(0));//Face the peg
+				runAction(new DriveStraightAction(0, 0, 0));//Drive onto the peg
+				//add code to go to the hopper after gear
+				//add code to shoot after hopper
 				break;
 			case RIGHTGEAR:
+				runAction(new DriveStraightAction(0, 0, 0));//Drive in front of the peg
+				runAction(new RotateAction(0));//Face the peg
+				runAction(new DriveStraightAction(0, 0, 0));//Drive onto the peg
+				//add code to go to the hopper after gear
+				//add code to shoot after hopper
 				break;
-			case REDHOPPER:
-				runAction(new FollowPathAction());
+			case LEFTHOPPER:
+				runAction(new DriveStraightAction(0, 0, 0));//Drive to the hopper area
+				runAction(new DriveStraightAction(0, team?0:0, team?0:0));//Drive into the hopper
+				//add code to shoot after hopper
 				break;
-			case BLUHOPPER:
-				runAction(new FollowPathAction());
+			case RIGHTHOPPER:
+				runAction(new DriveStraightAction(0, 0, 0));//Drive to the hopper area
+				runAction(new DriveStraightAction(0, team?0:0, team?0:0));//Drive into the hopper
+				//add code to shoot after hopper
 				break;
 		}
 	}
