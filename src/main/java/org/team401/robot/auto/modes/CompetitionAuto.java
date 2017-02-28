c274867774c2679f75d3944660c986247f79d381
Unknown <eridor@verizon.net>
2017-02-27 20:02:59 -0500

    Wrote FollowPathAction class from Eli's previous code

--- src/main/java/org/team401/robot/auto/modes/CompetitionAuto.java
+++ src/main/java/org/team401/robot/auto/modes/CompetitionAuto.java
@@ -3,6 +3,7 @@ package org.team401.robot.auto.modes;
 import org.team401.robot.auto.AutoMode;
 import org.team401.robot.auto.actions.CalibrateTurretAction;
 import org.team401.robot.auto.actions.DriveStraightAction;
+import org.team401.robot.auto.actions.FollowPathAction;
 import org.team401.robot.auto.actions.RotateAction;
 
 import edu.wpi.first.wpilibj.DriverStation;
@@ -25,8 +26,7 @@ public class CompetitionAuto extends AutoMode {
 		runAction(new CalibrateTurretAction());
 		switch(destination) {
 			case MIDGEAR:
-				runAction(new DriveStraightAction(0, 0, 0));//Drive onto the peg
-				findPath(AutoPaths.START_MID_TO_LIFT.getArr(), AutoPaths.START_MID_TO_LIFT.getTime());
+				runAction(new FollowPathAction(AutoPaths.START_MID_TO_LIFT.getArr(), AutoPaths.START_MID_TO_LIFT.getTime()));
 				break;
 			case LEFTGEAR:
 				runAction(new DriveStraightAction(0, 0, 0));//Drive in front of the peg
@@ -55,29 +55,6 @@ public class CompetitionAuto extends AutoMode {
 		}
 	}
 	public void done(){
-
-	}
-
-	/**
-	 * Method for translating the autopaths waypoints into Zachs methods.
-	 * @param path the autopath being used
-	 * @param time the time it takes to run it (should be part of the autopath)
-	 */
-
-	private void findPath(double[][] path, double time){
-		double speed = time/path.length;
-		for(int i = 0;i<path.length;i++){
-			if(Math.atan2(path[i][1], path[i][0]) != Math.atan2(path[i+1][1], path[i+1][0])){//a turn is required
-				runAction(new RotateAction(Math.toDegrees(Math.atan2(path[i+1][1], path[i+1][0]))));
-			}
-			//no turn needed
-			runAction(new DriveStraightAction(speed, findDistance(path[i], path[i+1]),Math.atan2(path[i][1], path[i][0])));
-		}
-
-	}
-
-	private double findDistance(double[] point1, double[] point2){
-		return Math.sqrt(Math.pow(point1[0] + point2[0],2) + Math.pow(point1[1] + point2[1],2));
 
 	}
 }
